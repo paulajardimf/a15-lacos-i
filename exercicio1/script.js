04f484d72a534fca822d36038c76c9562bf12cb3
Paula <paulajardimf@gmail.com>
2022-09-12 15:57:35 -0300

    correção

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,11 @@
 let conta = 0
 let coxinha = prompt("Você quer uma coxinha? [S/N]");
 let unidade = 0
+const valorCoxinha = 2.5
 
 while (coxinha.toUpperCase() == "S") {
     coxinha = prompt("Você deseja comer mais coxinhas? [S/N]");
-    conta += 2.5;
+    conta += valorCoxinha;
     unidade += 1;
 }
 console.log(`Você comeu ${unidade} coxinhas, e sua conta é no total de ${conta}`);
